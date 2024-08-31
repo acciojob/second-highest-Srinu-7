d716ede1f76a9f54e1e7a805d1ab63ffb391023b
Srinu-7 <srinumaka9908@gmail.com>
2024-08-31 20:57:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
 //your JS code here. If required.
-function secondHighest(arr) {
-    let 
-}
-
-// Example Usage:
-console.log(secondHighest([5, 1, 2, 3, 4]));  // Output: 4
-console.log(secondHighest([-1, -2, -3, -4, -5]));  // Output: -2
-console.log(secondHighest([]));  // Output: -Infinity
-console.log(secondHighest([1]));  // Output: -Infinity
-console.log(secondHighest([1, 1, 1, 1, 1]));  // Output: -Infinity
