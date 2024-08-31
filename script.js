0d2ebc1237c7283f7c559ec5b005cc9b33e30c77
Srinu-7 <srinumaka9908@gmail.com>
2024-08-31 20:56:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,6 @@
 //your JS code here. If required.
 function secondHighest(arr) {
-    if (arr.length < 2) {
-        return -Infinity;
-    }
-
-    let highest = -Infinity;
-    let secondHighest = -Infinity;
-
-    for (let num of arr) {
-        if (num > highest) {
-            secondHighest = highest;
-            highest = num;
-        } else if (num > secondHighest && num < highest) {
-            secondHighest = num;
-        }
-    }
-
-    return secondHighest === -Infinity ? -Infinity : secondHighest;
+    let 
 }
 
 // Example Usage:
